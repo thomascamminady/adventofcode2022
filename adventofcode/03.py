97f70fb880a0464af1ade6f54ca5bd8330d9afc2
Thomas Camminady <0milieux_member@icloud.com>
2022-12-03 08:31:16 +0100

    Day 3.

--- adventofcode/03.py
+++ adventofcode/03.py
@@ -3,15 +3,40 @@ from rich import print
 from adventofcode.helper.io import get_riddle_input, save_riddle_input
 
 
+def count(char: str):
+    # map a through z to 1 through 26
+    # and A through Z to 27 through 52
+    if char.islower():
+        return ord(char) - 96
+    else:
+        return ord(char) - 38
+
+
 def riddle1(riddle_input: str) -> int:
-    print(riddle_input)
     answer = 0
+    for line in riddle_input.splitlines():
+        n = len(line)
+        firsthalf = set(line[: n // 2])  # noqa: E203
+        secondhalf = set(line[n // 2 :])  # noqa: E203
+        commonchar = firsthalf.intersection(secondhalf)
+        assert len(commonchar) == 1
+        c = commonchar.pop()
+        answer += count(c)
+
     return answer
 
 
 def riddle2(riddle_input: str) -> int:
-    print(riddle_input)
     answer = 0
+    all_lines = riddle_input.splitlines()
+    # iterate in steps of 3
+    for i in range(0, len(all_lines), 3):
+        f, s, t = all_lines[i : i + 3]  # noqa: E203
+        intersection = set(f).intersection(set(s)).intersection(set(t))
+        assert len(intersection) == 1
+        c = intersection.pop()
+        answer += count(c)
+
     return answer
 
 
@@ -20,4 +45,4 @@ if __name__ == "__main__":
     save_riddle_input(__file__, riddle_input)
 
     print(riddle1(riddle_input))
-    # print(riddle2(get_riddle_input(__file__)))
+    print(riddle2(get_riddle_input(__file__)))
