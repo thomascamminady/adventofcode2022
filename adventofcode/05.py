33cb24da734a8b88c74a0bfa45bfecee3b19aa79
Thomas Camminady <0milieux_member@icloud.com>
2022-12-05 07:54:45 +0100

    Day 5.

--- adventofcode/05.py
+++ adventofcode/05.py
@@ -1,17 +1,50 @@
+import re
+
 from rich import print
 
 from adventofcode.helper.io import get_riddle_input, save_riddle_input
 
 
-def riddle1(riddle_input: str) -> int:
-    print(riddle_input)
-    answer = 0
+def riddle1(riddle_input: str) -> str:
+    lines = riddle_input.splitlines()[:8]
+    contents = [[] for _ in range(9)]
+    for li in lines:
+        c = li[1:-1:4]
+        for i, ci in enumerate(c):
+            if ci != " ":
+                contents[i].insert(0, ci)
+
+    for line in riddle_input.splitlines()[10:]:
+        if m := re.match(r"move (\d+) from (\w+) to (\w+)", line):
+            n, x, y = m.groups()
+            x, y, n = int(x) - 1, int(y) - 1, int(n)
+            for i in range(min(n, len(contents[x]))):
+                value = contents[x].pop()
+                contents[y].append(value)
+
+    answer = "".join([c[-1] for c in contents if c])
     return answer
 
 
-def riddle2(riddle_input: str) -> int:
-    print(riddle_input)
-    answer = 0
+def riddle2(riddle_input: str) -> str:
+    lines = riddle_input.splitlines()[:8]
+    contents = [[] for _ in range(9)]
+    for li in lines:
+        c = li[1:-1:4]
+        for i, ci in enumerate(c):
+            if ci != " ":
+                contents[i].insert(0, ci)
+
+    for line in riddle_input.splitlines()[10:]:
+        if m := re.match(r"move (\d+) from (\w+) to (\w+)", line):
+            n, x, y = m.groups()
+            x, y, n = int(x) - 1, int(y) - 1, int(n)
+
+            values = contents[x][-n:]
+            contents[x] = contents[x][:-n]
+            contents[y].extend(values)
+
+    answer = "".join([c[-1] for c in contents if c])
     return answer
 
 
@@ -20,4 +53,4 @@ if __name__ == "__main__":
     save_riddle_input(__file__, riddle_input)
 
     print(riddle1(riddle_input))
-    # print(riddle2(get_riddle_input(__file__)))
+    print(riddle2(get_riddle_input(__file__)))
