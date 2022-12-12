ab68424ceef501052c3100e5eed10fec61aa6332
Thomas Camminady <0milieux_member@icloud.com>
2022-12-12 18:06:29 +0100

    Update template.

--- adventofcode/20.py
+++ adventofcode/20.py
@@ -1,21 +1,26 @@
-from rich import print
+from __future__ import annotations
+
+import numpy as np
+from rich import inspect, print
 
 from adventofcode.helper.io import get_day, get_riddle_input, save_riddle_input
 
 
 def riddle1(riddle_input: str) -> int | str:
-    print(riddle_input)
-
     answer = 0
+
     for i, line in enumerate(riddle_input.splitlines()):
         pass
+
     return answer
 
 
 def riddle2(riddle_input: str) -> int | str:
     answer = 0
+
     for i, line in enumerate(riddle_input.splitlines()):
         pass
+
     return answer
 
 
@@ -24,11 +29,11 @@ if __name__ == "__main__":
     riddle_input = get_riddle_input(day)
     save_riddle_input(day, riddle_input)
 
-    example = """"""
-    if len(example) > 0:
-        answer1 = riddle1(example)
-    else:
-        answer1 = riddle1(riddle_input)
+    # placeholder for example
+    # riddle_input = """"""
+
+    print(riddle_input)
+    answer1 = riddle1(riddle_input)
     print(answer1)
 
     if answer1 != 0:
